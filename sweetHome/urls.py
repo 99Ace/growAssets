b19564ec34e15c15f3965a233ca6d4a85c949146
99Ace <chunliang.ang@gmail.com>
2021-03-24 19:51:19 +0000

    link to mongo

--- sweetHome/urls.py
+++ sweetHome/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import sweethome
+from .views import sweethome, new_expenses
 
 urlpatterns = [
     path('', sweethome, name='sweethome'),
+    path('new_expenses/', new_expenses, name='new_expenses'),
 ]
