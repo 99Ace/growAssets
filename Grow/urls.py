4c8f93d6b862c0742339c19390211093ae229172
99Ace <chunliang.ang@gmail.com>
2021-04-24 01:34:56 +0000

    update path

--- Grow/urls.py
+++ Grow/urls.py
@@ -20,6 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # path('', include('menu.urls')),
     # path('sweethome/', include('sweetHome.urls')),
-    path('mikar9/', include('mikar9.urls')),
+    path('', include('mikar9.urls')),
     # path('happyCoders/', include('happyCoders.urls')),
 ]
